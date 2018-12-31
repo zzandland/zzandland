5579aeb44adf4286add5e8154b1b031742bceeb2
Si Yong Kim <zzandland@gmail.com>
2018-12-30 18:25:19 -0800

    polish up for deployment

--- client/components/Modals/AboutSubComponents/FunFacts.jsx
+++ client/components/Modals/AboutSubComponents/FunFacts.jsx
@@ -5,24 +5,24 @@ export default props => (
     <div className="row">
       <div className="col s12 m4 l4 center-align">
         <h3>
-          <i className="fa fa-briefcase"></i>
-					<span className="font-weight-900">4+</span>
-        </h3>
-        <h6 className="uppercase font-weight-700">Years Experience</h6>
-      </div>
-      <div className="col s12 m4 l4 center-align">
-        <h3>
           <i className="far fa-handshake"></i>
-          <span className="font-weight-900">89+</span>
+          <span className="font-weight-900">4+</span>
         </h3>
         <h6 className="uppercase font-weight-700">Done Projects</h6>
       </div>
       <div className="col s12 m4 l4 center-align">
         <h3>
-          <i className="far fa-heart"></i>
-          <span className="font-weight-900">77+</span>
+          <i className="fab fa-linux"></i>
+					<span className="font-weight-900">5</span>
         </h3>
-        <h6 className="uppercase font-weight-700">Happy customers</h6>
+        <h6 className="uppercase font-weight-700">Tried Linux Distros</h6>
+      </div>
+      <div className="col s12 m4 l4 center-align">
+        <h3>
+          <i className="fas fa-drum"></i>
+          <span className="font-weight-900">10+</span>
+        </h3>
+        <h6 className="uppercase font-weight-700">Years Playing Drums</h6>
       </div>
     </div>
   </div>
