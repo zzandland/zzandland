88f910ef0acdd7ebc1b590f89a0a7694bbe20407
Si Yong Kim <zzandland@gmail.com>
2020-09-07 23:47:34 -0400

    Add showdown and use it to load markdown files

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -5,14 +5,30 @@
       header-level="5"
       lead="The best time to plant a tree is twenty years ago. The second best time is now."
     />
+    <VueShowdown :markdown="test" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import showdown from 'showdown';
+import test from '../assets/articles/2020-09-07.md';
+
+showdown.setFlavor('github');
 
 @Component
-export default class Articles extends Vue {}
+export default class Articles extends Vue {
+  data: string[] = [];
+
+  created() {
+    this.convert();
+  }
+
+  convert() {
+    const converter = new showdown.Converter();
+    this.data.push(converter.makeHtml(test));
+  }
+}
 </script>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 import './assets/styles/bootstrap.min.css';
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import VueShowdown from 'vue-showdown';
 import App from './App.vue';
 import Articles from './components/Articles.vue';
 import Projects from './components/Projects.vue';
@@ -12,10 +11,6 @@ import Projects from './components/Projects.vue';
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(VueRouter);
-Vue.use(VueShowdown, {
-  flavor: 'github',
-  options: { emoji: true },
-});
 
 const routes: RouteConfig[] = [
   { path: '/', component: Articles, name: 'Articles' },
