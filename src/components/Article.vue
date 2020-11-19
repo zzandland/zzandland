5ccba6d5a386f3ecf7e88debe58db50646726cb8
Si Yong Kim <zzandland@gmail.com>
2020-11-19 12:12:44 -0500

    OS study chapter 9 complete

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -58,134 +58,133 @@ export default class Article extends Vue {
 </script>
 
 <style scoped>
-  a.btn {
-    color: rgb(211, 211, 211);
-    padding: 0;
+a.btn {
+  color: rgb(211, 211, 211);
+  padding: 0;
+}
+
+span {
+  color: lightgrey;
+}
+
+.article {
+  font-size: 0.9rem;
+}
+
+.article >>> blockquote {
+  margin-left: 0.5rem;
+  margin-right: 0.5rem;
+  border-left: 3px solid #f36d33;
+  padding-left: 1rem;
+  color: darkgrey !important;
+}
+
+.article >>> blockquote * {
+  font-size: 0.8rem !important;
+}
+
+.article >>> hr {
+  background-color: #f36d33;
+  margin-bottom: 3rem;
+  height: 1px;
+}
+
+.article >>> h1, .article >>> h3, .article >>> h4 {
+  margin-top: 3rem;
+  margin-bottom: 1.5rem;
+}
+
+.article >>> h1 {
+  font-size: 2.3rem;
+}
+
+.article >>> h2 {
+  font-size: 1.4rem;
+  color: grey;
+}
+
+.article >>> h3 {
+  font-size: 1.8rem;
+}
+
+.article >>> h4 {
+  font-size: 1.6rem;
+}
+
+.article >>> img {
+  width: 100%;
+  height: 100%;
+  margin-bottom: 2rem;
+}
+
+.article >>> ol, .article >>> ul {
+  padding-left: 2rem;
+}
+
+.article >>> ol p, .article >>> ul p {
+  margin-bottom: 0;
+}
+
+.article >>> p {
+  margin-bottom: 1.5rem;
+}
+
+.article >>> pre {
+  background-color: #282828;
+  border: 1px solid #2e2e2e;
+  border-left: 3px solid #f36d33;
+  page-break-inside: avoid;
+  font-size: 15px;
+  line-height: 1.6;
+  margin: 0 0.5rem 1.6rem 0.5rem;
+  max-width: 100%;
+  overflow: auto;
+  padding: 1em 1.5em;
+  display: block;
+  word-wrap: break-word;
+}
+
+.article >>> pre code {
+  font-family: 'Ubuntu Mono', monospace;
+}
+
+@media (max-width: 768px) {
+  div.mx-auto.my-5.container {
+    margin-top: 0;
   }
 
-  span {
-    color: lightgrey;
+  a.btn, span {
+    font-size: 0.8rem;
   }
 
   .article {
-    font-size: 0.9rem;
-  }
-
-  .article >>> blockquote {
-    margin-left: 0.5rem;
-    margin-right: 0.5rem;
-    border-left: 3px solid #f36d33;
-    padding-left: 1rem;
-    color: darkgrey !important;
-  }
-
-  .article >>> blockquote * {
-    font-size: 0.8rem !important;
-  }
-
-  .article >>> hr {
-    background-color: #f36d33;
-    margin-bottom: 3rem;
-    height: 1px;
-  }
-
-  .article >>> h1, .article >>> h3, .article >>> h4 {
-    margin-top: 3rem;
-    margin-bottom: 1.5rem;
+    font-size: 0.8rem;
   }
 
   .article >>> h1 {
-    font-size: 2.3rem;
-  }
-
-  .article >>> h2 {
-    font-size: 1.4rem;
-    color: grey;
-  }
-
-  .article >>> h3 {
     font-size: 1.8rem;
   }
 
-  .article >>> h4 {
+  .article >>> h2 {
+    font-size: 1.3rem !important;
+  }
+
+  .article >>> h3 {
     font-size: 1.6rem;
   }
 
-  .article >>> img {
-    width: 100%;
-    height: 100%;
-    margin-bottom: 2rem;
+  .article >>> h4 {
+    font-size: 1.4rem;
   }
 
   .article >>> ol, .article >>> ul {
-    padding-left: 2rem;
+    padding-left: 1rem;
   }
+}
 
-  .article >>> ol p, .article >>> ul p {
-    margin-bottom: 0;
+@media (min-width: 768px) {
+  .container {
+    width: 65%;
   }
-
-  .article >>> p {
-    margin-bottom: 1.5rem;
-  }
-
-  .article >>> pre {
-    background-color: #282828;
-    border: 1px solid #2e2e2e;
-    border-left: 3px solid #f36d33;
-    page-break-inside: avoid;
-    font-size: 15px;
-    line-height: 1.6;
-    margin: 0 0.5rem 1.6rem 0.5rem;
-    max-width: 100%;
-    overflow: auto;
-    padding: 1em 1.5em;
-    display: block;
-    word-wrap: break-word;
-  }
-
-  .article >>> pre code {
-    font-family: 'Ubuntu Mono', monospace;
-  }
-
-  @media (max-width: 768px) {
-    div.mx-auto.my-5.container {
-      margin-top: 0;
-    }
-
-    a.btn, span {
-      font-size: 0.8rem;
-    }
-
-    .article {
-      font-size: 0.8rem;
-    }
-
-    .article >>> h1 {
-      font-size: 1.8rem;
-    }
-
-    .article >>> h2 {
-      font-size: 1.3rem !important;
-    }
-
-    .article >>> h3 {
-      font-size: 1.6rem;
-    }
-
-    .article >>> h4 {
-      font-size: 1.4rem;
-    }
-
-    .article >>> ol, .article >>> ul {
-      padding-left: 1rem;
-    }
-  }
-
-  @media (min-width: 768px) {
-    .container {
-      width: 65%;
-    }
-  }
-
+}
 </style>
