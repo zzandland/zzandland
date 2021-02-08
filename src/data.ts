dd4b253ed6d5e19571c37b2c1144f875fffd2545
Si Yong Kim <zzandland@gmail.com>
2021-02-07 22:21:13 -0800

    Change project image src to be directly pulled from preview of the github repo

--- src/data.ts
+++ src/data.ts
@@ -1,8 +1,20 @@
 export default [
   {
     title: 'pyqt-sort',
-    imgUrl: 'https://s3-us-west-1.amazonaws.com/zzandland.io/assets/previews/pyqt_sort_preview.gif',
+    imgUrl: 'https://github.com/zzandland/pyqt-sort/blob/master/preview.gif?raw=true',
     description: 'Built using PyQt5, the program visualizes 10 sorting algorithms.',
     path: 'https://github.com/zzandland/pyqt-sort',
+  },
+  {
+    title: 'zbar',
+    imgUrl: 'https://github.com/zzandland/zbar/blob/main/preview.png?raw=true',
+    description: 'Performance oriented Übersicht widget status bar for yabai tiling WM.',
+    path: 'https://github.com/zzandland/zbar',
+  },
+  {
+    title: 'Jinenertech',
+    imgUrl: 'https://raw.githubusercontent.com/Jinenertech/jinenertech/master/preview.png?token=AGNIVV4TRSTGI6JPIDAEHH3AEDMAS',
+    description: 'Official website for company Jinenertech Co., Ltd.',
+    path: 'https://www.jinenertech.co.kr',
   },
 ];
